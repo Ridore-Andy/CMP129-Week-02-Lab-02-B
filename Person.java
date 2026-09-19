4f5bb9493f5e9958544f67be1476a7bb190c4af6
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-18 21:44:45 -0400

    Complete Week 2 lab 2

--- Person.java
+++ Person.java
@@ -1,3 +1,54 @@
-public class Person {
+public class Person 
+{
+    //setting attributes
+    private String name;
+    private int age;
+    private String email;
+
+    //constructor
+    public Person()
+    {
+
+    }
+
+    //get and set name
+    public String getName(String name)
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    //get and set age
+     public int getAge(int age) 
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    //get and set email
+     public String getEmail(String email)
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    //display person info
+    public void displayInfo()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Email: "+email);
+    }
     
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,29 @@
-public class PersonTest {
+public class PersonTest 
+{
+    public static void main(String[] args) 
+    {
+        //person 1
+        Person p1 = new Person();
+
+        //setting person 1 values
+        p1.setName("Jimmy");
+        p1.setAge(31);
+        p1.setEmail("jimmy@gmail.com");
+        //person 1 display
+        System.out.println("Person 1 Information:");
+        p1.displayInfo();
+
+        //person 2
+        Person p2 = new Person();
+
+        //setting person 2 values
+        p2.setName("Carl");
+        p2.setAge(94);
+        p2.setEmail("carl@gmail.com");
+        //person 2 display
+        System.out.println("\nPerson 2 Information:");
+        p2.displayInfo();
+        
+    }
     
 }
